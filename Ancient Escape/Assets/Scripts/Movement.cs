9a1926dedfba9d63519ea9e0f989c02c086b0790
WAVEBREAKER\NateBlair <nategribble@gmail.com>
2017-02-20 12:37:18 -0500

    Initial commit for first 3 levels.

---
+++ Ancient Escape/Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Movement : MonoBehaviour {
+
+    public bool canMoveVertical;
+    public bool canMoveHorizontal;
+    public float width;
+    public float height;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void Update()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        var yInput = Input.GetAxis("Mouse Y");
+        var xInput = Input.GetAxis("Mouse X");
+        var movingHorizontal = true;
+        var movingVertical = true;
+        Vector3 raycastDirection;
+        float castDistance = 0;
+        // Decide if the block is trying to move up and down or left and right.
+        if (Math.Abs(yInput) > Math.Abs(xInput))
+        {
+            movingHorizontal = false;
+            castDistance = height / 2;
+
+            if (yInput > 0)
+                raycastDirection = Vector3.up;
+            else
+                raycastDirection = Vector3.down;
+        }
+        else
+        {
+            movingVertical = false;
+            castDistance = width / 2;
+            if (xInput > 0)
+                raycastDirection = Vector3.right;
+            else
+                raycastDirection = Vector3.left;
+        }
+
+        // Block movement in directions a block should not go
+        //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        if (!canMoveHorizontal || !movingHorizontal)
+        {
+            curPosition = new Vector3(gameObject.transform.position.x, curPosition.y, curPosition.z);
+        }
+        if (!canMoveVertical || !movingVertical)
+        {
+            curPosition = new Vector3(curPosition.x, gameObject.transform.position.y, curPosition.z);
+        }
+        
+        //Is a block about to hit another block?
+        var hits = Physics2D.RaycastAll(gameObject.transform.position, raycastDirection, castDistance);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit && hit.transform.gameObject != gameObject)
+            {
+                return;
+            }
+        }
+
+        // Update position
+        transform.position = curPosition;
+    }
+}
---
+++ Ancient Escape/Assets/Scripts/PushPower.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PushPower : MonoBehaviour {
+
+    public int numberCastable;
+    public float radius;
+    public float distance;
+    public Text castableButton;
+
+    void Start ()
+    {
+        castableButton.text = numberCastable.ToString();
+    }
+
+    private bool open;
+	public void PushBlocks()
+    {
+        //Is a block about to hit another block?
+        if (numberCastable > 0)
+        {
+            // Check right
+            open = true;
+            List<RaycastHit2D> hits = new List<RaycastHit2D>();
+            hits.AddRange(Physics2D.RaycastAll(gameObject.transform.position, Vector3.right, radius));
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit && hit.transform.gameObject != gameObject && hit.transform.gameObject.tag == "Block")
+                {
+                    var blockHits = Physics2D.RaycastAll(hit.transform.position, Vector3.right, hit.transform.GetComponent<Movement>().width/2);
+                    foreach (RaycastHit2D innerHit in blockHits)
+                    {
+                        if (innerHit && innerHit.transform.gameObject != hit.transform.gameObject)
+                            open = false;
+                    }
+
+                    if (open && hit.transform.gameObject.tag == "Block")
+                        hit.transform.position = new Vector3(hit.transform.position.x + distance, hit.transform.position.y, hit.transform.position.z);
+                }
+            }
+
+            // Check left
+            open = true;
+            hits.Clear();
+            hits.AddRange(Physics2D.RaycastAll(gameObject.transform.position, Vector3.left, radius));
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit && hit.transform.gameObject != gameObject && hit.transform.gameObject.tag == "Block")
+                {
+                    var blockHits = Physics2D.RaycastAll(hit.transform.position, Vector3.left, hit.transform.GetComponent<Movement>().width / 2);
+                    foreach (RaycastHit2D innerHit in blockHits)
+                    {
+                        if (innerHit && innerHit.transform.gameObject != hit.transform.gameObject)
+                            open = false;
+                        ;
+                    }
+
+                    if (open && hit.transform.gameObject.tag == "Block")
+                        hit.transform.position = new Vector3(hit.transform.position.x - distance, hit.transform.position.y, hit.transform.position.z);
+                }
+            }
+
+            // Check up
+            open = true;
+            hits.Clear();
+            hits.AddRange(Physics2D.RaycastAll(gameObject.transform.position, Vector3.up, radius));
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit && hit.transform.gameObject != gameObject && hit.transform.gameObject.tag == "Block")
+                {
+                    var blockHits = Physics2D.RaycastAll(hit.transform.position, Vector3.up, hit.transform.GetComponent<Movement>().height / 2);
+                    foreach (RaycastHit2D innerHit in blockHits)
+                    {
+                        if (innerHit && innerHit.transform.gameObject != hit.transform.gameObject)
+                            open = false;
+                    }
+
+                    if (open && hit.transform.gameObject.tag == "Block")
+                        hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + distance, hit.transform.position.z);
+                }
+            }
+
+            // Check down
+            open = true;
+            hits.Clear();
+            hits.AddRange(Physics2D.RaycastAll(gameObject.transform.position, Vector3.down, radius));
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit && hit.transform.gameObject != gameObject && hit.transform.gameObject.tag == "Block")
+                {
+                    var blockHits = Physics2D.RaycastAll(hit.transform.position, Vector3.down, hit.transform.GetComponent<Movement>().height / 2);
+                    foreach (RaycastHit2D innerHit in blockHits)
+                    {
+                        if (innerHit && innerHit.transform.gameObject != hit.transform.gameObject)
+                            open = false;
+                    }
+
+                    if (open && hit.transform.gameObject.tag == "Block")
+                        hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y - distance, hit.transform.position.z);
+                }
+            }
+
+            numberCastable -= 1;
+            castableButton.text = numberCastable.ToString();
+        }
+    }
+}
